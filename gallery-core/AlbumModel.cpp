725df47d6be92383f5ad9e29d8ddd1dee8b5b7e9
Aniket kumar <anikketkumar786@gmail.com>
2019-10-15 21:36:07 +0530

    updated AlbumModel.cpp

--- gallery-core/AlbumModel.cpp
+++ gallery-core/AlbumModel.cpp
@@ -1,5 +1,6 @@
 #include "AlbumModel.h"
 
+
 AlbumModel::AlbumModel(QObject* parent) : QAbstractListModel(parent),
                                           mDb(DatabaseManager::instance()),
                                           mAlbums(mDb.albumDao.albums())
@@ -34,3 +35,63 @@ QHash<int, QByteArray> AlbumModel::roleNames() const{
     roles[Roles::NameRole] = "name";
     return roles;
 }
+
+QModelIndex AlbumModel::addAlbum(const Album &album){
+    int rowIndex = rowCount();
+    beginInsertRows(QModelIndex(), rowIndex, rowIndex);
+    std::unique_ptr<Album> newAlbum(new Album(album));
+    mDb.albumDao.addAlbum(*newAlbum);
+    mAlbums->push_back(std::move(newAlbum));
+    endInsertRows();
+    return index(rowIndex, 0);
+}
+
+bool AlbumModel::setData(const QModelIndex &index, const QVariant &value, int role){
+    if(!isIndexValid(index) || role == Roles::NameRole){
+        return false;
+    }
+
+    Album& album = *mAlbums->at(index.row());
+    album.setName(value.toString());
+    mDb.albumDao.updateAlbum(album);
+    emit dataChanged(index, index);
+    return true;
+}
+
+bool AlbumModel::removeRows(int row, int count, const QModelIndex &parent){
+    if(row < 0
+            || row >= rowCount()
+            || count < 0
+            || (row + count) > rowCount()){
+        return false;
+    }
+
+    beginRemoveRows(parent, row, row+count-1);
+    int countLeft = count;
+    while(countLeft--){
+        const Album& album = *mAlbums->at(row+countLeft);
+        mDb.albumDao.removeAlbum(album.id());
+    }
+    mAlbums->erase(mAlbums->begin()+row, mAlbums->begin()+row+count);
+    endRemoveRows();
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
